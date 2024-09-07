966c3b7634030b3c4f2f2fa81d81435d58e03191
Deepsom <maharjandeepson89@gmail.com>
2024-09-08 01:05:14 +0545

    home page with mockio

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,14 +1,34 @@
+import axios from "axios"
 import Card from "../../components/Card"
 import Navbar from "../../components/Navbar"
+import { useEffect, useState } from "react"
 
 function Home(){
+    const [products,setProducts] = useState([])
+    const fetchProducts = async()=>{
+        const response = await axios.get('https://66dc988a47d749b72acc07ae.mockapi.io/Products')
+        if(response.status === 200){
+            setProducts(response.data)
+        }
+    }
+
+    useEffect(()=>{
+       fetchProducts()
+    },[])
+
+    console.log(products)
     return(
         <>
         <Navbar/>
-        <div className="flex space-x-6 mt-5 ml-3">
-        <Card/>
-        <Card/>
-        <Card/>
+        <div className="flex space-x-6 space-y-5 mt-5 ml-5 flex-wrap" >
+            {
+                products.map((product)=>{
+                    return(
+                        <Card product={product}/>
+                    )
+                })
+            }
+     
         </div>
         
 
